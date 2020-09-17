aec2695e56706221106ff5e1986a5c8c7b6c9327
Erick Valdez <67769889+ErickValdez26@users.noreply.github.com>
2020-09-17 17:23:36 -0500

    ListarTragos

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
+import ListaRecetas from './components/ListaRecetas';
 
 import CategoriasProvider from './context/CategoriasContext';
+import RecetasContext from './context/RecetasContext';
+import RecetasProvider from './context/RecetasContext';
 
 function App() {
   return (
     <CategoriasProvider>
-      <Header />
+      <RecetasProvider>
+        <Header />
       
-      <div className="container mt-5">
-        <div className="row">
-          <Formulario />
+        <div className="container mt-5">
+          <div className="row">
+            <Formulario />
+          </div>
+          
+          <ListaRecetas />
         </div>
-      </div>
+      </RecetasProvider>
     </CategoriasProvider>
   );
 }
